4133a7e6ac9b1d60a4ef20d9637c9414e3d22d0f
Peter Dalmaris <peter@futureshock.com.au>
2020-01-07 09:38:33 +1100

    Updated to work with the current RF24 library
    Tested against the current RF24 library on January 7, 2020.

--- RF24_demo/receiver/receiver.ino
+++ RF24_demo/receiver/receiver.ino
@@ -4,12 +4,7 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-class RF24Test: public RF24
-{
-public: RF24Test(int a, int b): RF24(a,b) {}
-};
-
-RF24Test radio(9,10);
+RF24 radio(9, 10);
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
@@ -17,48 +12,46 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 void setup()
 {
   Serial.begin(9600);
-  
-  radio.begin();  
+
+  radio.begin();
   radio.openWritingPipe(pipes[1]);
-  radio.openReadingPipe(1,pipes[0]);
-  radio.startListening();  
+  radio.openReadingPipe(1, pipes[0]);
+  radio.startListening();
   Serial.println("Listening");
 }
 
 void loop()
 {
-  
-    // if there is data ready
-    if ( radio.available() )
-    {
-      Serial.print("Receiver.");
-      char transmission;
-      bool done = false;
-      while (!done)
-      {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read( &transmission, 1 );
+  // if there is data ready
+  if ( radio.available() )
+  {
+    Serial.print("Receiver.");
+    char transmission;
+    bool done = false;
 
-        // Spew it
-        Serial.print("Received from transmitter:");
-        Serial.println(transmission);
- 
-  // Delay just a little bit to let the other unit
-  // make the transition to receiver
-  delay(20);
-      }
+    while (radio.available()) {
+      radio.read( &transmission, 1 );
+    }
 
-      // First, stop listening so we can talk
-      radio.stopListening();
+    // Spew it
+    Serial.print("Received from transmitter:");
+    Serial.println(transmission);
 
-      // Send the final one back.
-      byte response = B0;
-      radio.write( &response, sizeof(response) );
-      Serial.println("Sent response.\n\r");
+    // Delay just a little bit to let the other unit
+    // make the transition to receiver
+    delay(20);
 
-      // Now, resume listening so we catch the next packets.
-      radio.startListening();
+    // First, stop listening so we can talk
+    radio.stopListening();
 
-    }  
-  
+    // Send the final one back.
+    byte response = B0;
+    radio.write( &response, sizeof(response) );
+    Serial.println("Sent response.\n\r");
+
+    // Now, resume listening so we catch the next packets.
+    radio.startListening();
+
+  }
+
 }
